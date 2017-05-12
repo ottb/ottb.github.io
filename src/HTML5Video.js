4dc53af1cc4356d62146a74eb69184160049a582
Brian Ott <ottb@ccf.org>
2017-05-12 08:41:52 -0400

    Tweaked video styling

--- src/HTML5Video.js
+++ src/HTML5Video.js
@@ -3,14 +3,17 @@ import './styles.css';
 
 class HTML5Video extends Component {
   render() {
+		const videoStyle = {
+        bottom: "80px"
+    };
     return (
-      <div id="js-video-wrapper" className="video-wrapper">
+      <div id="js-video-wrapper" className="video-wrapper" style={ videoStyle }>
 				<video id="js-video" className="video" controls preload playsInline autoPlay loop poster="img/2016-ar-bg.png">
 					<source src="video/ar-2016-video.mp4" type="video/mp4" />
 					<source src="video/ar-2016-video.webm" type="video/webm" />
 					Your browser does not support HTML5 video.
 				</video>
-				<div>				
+				<div id="js-video-controls" className="video-controls">				
 					<div id="js-mute"><img className="sound-on" alt="Mute" src="img/video-sound-on.svg"/></div>
 					<div id="js-play"><img className="play" alt="Play" src="img/video-play.svg"/></div>
 					<div id="js-full-screen"><img className="reg-screen" alt="Full Screen" src="img/video-reg-screen.svg"/></div>
--- src/KalturaVideo.js
+++ src/KalturaVideo.js
@@ -8,7 +8,7 @@ class KalturaVideo extends Component {
     };
     return (
         <div id="js-kaltura-wrapper" className="kaltura-video__container">
-            <div class="kaltura-video__dummy"></div>
+            <div className="kaltura-video__dummy"></div>
             <script src="https://cdnapisec.kaltura.com/p/2207941/sp/220794100/embedIframeJs/uiconf_id/37292221/partner_id/2207941"></script>
             <div id="kaltura_player_e30d2723-221d-4333-814f-d58fc8192b61" className="kWidgetIframeContainer kaltura-video__video" style={ videoStyle }>
             </div>
