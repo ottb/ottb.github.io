ce69dee6fdde1a78ead223380d23580d29d6126f
Brian Ott <ottb@ccf.org>
2017-05-12 08:18:27 -0400

    Removed display none from kaltura video

--- src/KalturaVideo.js
+++ src/KalturaVideo.js
@@ -3,14 +3,11 @@ import './styles.css';
 
 class KalturaVideo extends Component {
   render() {
-    const wrapperStyle = {
-        display: "none"
-    };
     const videoStyle = {
         overflow: "hidden"
     };
     return (
-        <div id="js-kaltura-wrapper" className="kaltura-video__container" style={ wrapperStyle }>
+        <div id="js-kaltura-wrapper" className="kaltura-video__container">
             <div class="kaltura-video__dummy"></div>
             <script src="https://cdnapisec.kaltura.com/p/2207941/sp/220794100/embedIframeJs/uiconf_id/37292221/partner_id/2207941"></script>
             <div id="kaltura_player_e30d2723-221d-4333-814f-d58fc8192b61" className="kWidgetIframeContainer kaltura-video__video" style={ videoStyle }>
